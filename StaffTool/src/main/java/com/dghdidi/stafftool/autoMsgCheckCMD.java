f373266d117e76f455df36eb012dd1b7d2f32108
CryingObsidian-CO <54172963+CryingObsidian-CO@users.noreply.github.com>
2023-07-28 09:35:58 +0800

    add CachedThreadPool fixed Bugs that don't stop

--- StaffTool/src/main/java/com/dghdidi/stafftool/autoMsgCheckCMD.java
+++ StaffTool/src/main/java/com/dghdidi/stafftool/autoMsgCheckCMD.java
@@ -6,17 +6,15 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.plugin.TabExecutor;
-import net.md_5.bungee.api.scheduler.ScheduledTask;
 
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Callable;
 
+import static com.dghdidi.stafftool.AsyncMsgCheck.executorService;
 import static com.dghdidi.stafftool.tpCMD.getStrings;
 
 public class autoMsgCheckCMD extends Command implements TabExecutor {
 
     private final Plugin plugin;
-    private int counter;
-    private ScheduledTask task;
     private ProxiedPlayer player, staff;
     private String QQ, targetID;
 
@@ -43,26 +41,39 @@ public class autoMsgCheckCMD extends Command implements TabExecutor {
             sender.sendMessage(new TextComponent("§c当前玩家不在线或不存在!"));
             return;
         }
-        counter = 0;
-        task = plugin.getProxy().getScheduler().schedule(plugin, this::runTask, 0, 20, TimeUnit.SECONDS);
+
+        executorService.submit((Callable<Void>) () -> {
+            runTask(player, staff, QQ, targetID);
+            return null;
+        });
+
+
     }
 
-    private void runTask() {
-        counter++;
-        if (plugin.getProxy().getPlayer(targetID) == null) {
-            staff.sendMessage(new TextComponent("§a§l玩家§e§l" + targetID + "§c§l已经离线§7(视为拒绝查端), §a§l请执行处罚"));
-            plugin.getProxy().getPluginManager().dispatchCommand(staff, "punish " + targetID);
-            task.cancel();
-        } else {
-            staff.sendMessage(new TextComponent("§a§l已向玩家§e§l" + targetID + "§a§l发送查端信息 §7(" + counter + "/20)!"));
+    private void runTask(ProxiedPlayer _player, ProxiedPlayer _staff, String _QQ, String _targetID) {
+        int counter = 1;
+
+
+        while (counter <= 20) {
+            if (plugin.getProxy().getPlayer(_targetID) == null) {
+                _staff.sendMessage(new TextComponent("§a§l玩家§e§l" + _targetID + "§c§l已经离线§7(视为拒绝查端), §a§l请执行处罚"));
+                plugin.getProxy().getPluginManager().dispatchCommand(_staff, "punish " + _targetID);
+                return;
+            } else {
+                _staff.sendMessage(new TextComponent("§a§l已向玩家§e§l" + _targetID + "§a§l发送查端信息 §7(" + counter + "/20)!"));
+            }
+            _player.sendMessage(new TextComponent("§cInf Staff §8> §f您的游戏行为被§e§l检测异常§f, 为创造良好的游戏环境, 需要对您的客户端进行检查," + "请您§e在五分钟内§f添加工作人员QQ: §b" + _QQ + ", §e§l无视或退出服务器§f将会被视为作弊§c§l封禁§f, 感谢您的理解和配合!"));
+            counter++;
+            try {
+                Thread.sleep(15000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        player.sendMessage(new TextComponent("§cInf Staff §8> §f您的游戏行为被§e§l检测异常§f, 为创造良好的游戏环境, 需要对您的客户端进行检查," +
-                "请您§e在五分钟内§f添加工作人员QQ: §b" + QQ + ", §e§l无视或退出服务器§f将会被视为作弊§c§l封禁§f, 感谢您的理解和配合!"));
-        if (counter >= 15) {
-            staff.sendMessage(new TextComponent("§a§l查端信息已发送完毕 请判断是否施加处罚"));
-            task.cancel();
-        }
+
+        _staff.sendMessage(new TextComponent("§a§l查端信息已发送完毕 请判断是否施加处罚"));
     }
+
 
     @Override
     public Iterable<String> onTabComplete(CommandSender sender, String[] args) {
