4e5b1443b30b28e1ff9c4bf288de575cdf0a2f4a
WangYuDi <105062809+DGHDidi@users.noreply.github.com>
2023-07-31 11:47:33 +0800

    Add files via upload

--- StaffTool/src/main/java/com/dghdidi/stafftool/PunishCMD.java
+++ StaffTool/src/main/java/com/dghdidi/stafftool/PunishCMD.java
@@ -2,21 +2,18 @@ package com.dghdidi.stafftool;
 
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.*;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.plugin.TabExecutor;
-
 
 import static com.dghdidi.stafftool.TpCMD.getStrings;
 
 public class PunishCMD extends Command implements TabExecutor {
 
     private final Plugin plugin;
+
     public PunishCMD(Plugin plugin) {
         super("punish", "staff.punish");
         this.plugin = plugin;
@@ -37,30 +34,18 @@ public class PunishCMD extends Command implements TabExecutor {
         ProxiedPlayer player = (ProxiedPlayer) sender;
         plugin.getProxy().getPluginManager().dispatchCommand(player, "history " + ID);
         BaseComponent LineOne = new TextComponent("封禁类:");
-        BaseComponent[] BAN_7D = new ComponentBuilder("[7D]")
-                .color(ChatColor.RED)
-                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/mp ban " + ID + " 7d 使用第三方软件破坏游戏平衡"))
-                .create();
-        BaseComponent[] BAN_14D = new ComponentBuilder("[14D]")
-                .color(ChatColor.RED)
-                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/mp ban " + ID + " 14d 使用第三方软件破坏游戏平衡"))
-                .create();
-        BaseComponent[] BAN_30D = new ComponentBuilder("[30D]")
-                .color(ChatColor.RED)
-                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/mp ban " + ID + " 30d 使用第三方软件破坏游戏平衡"))
-                .create();
-        BaseComponent[] BAN_90D = new ComponentBuilder("[90D]")
-                .color(ChatColor.RED)
-                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/mp ban " + ID + " 90d 使用第三方软件破坏游戏平衡"))
-                .create();
-        BaseComponent[] BAN_360D = new ComponentBuilder("[360D]")
-                .color(ChatColor.RED)
-                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/mp ban " + ID + " 360d 使用第三方软件破坏游戏平衡"))
-                .create();
-        BaseComponent[] BAN_PERM = new ComponentBuilder("[永久]")
-                .color(ChatColor.RED)
-                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/mp ban " + ID + " 使用第三方软件破坏游戏平衡"))
-                .create();
+        BaseComponent[] BAN_7D = createClickableText
+                ("[7D]", "/mp ipban " + ID + " 7d 使用第三方软件破坏游戏规则", "§e点击封禁7天");
+        BaseComponent[] BAN_14D = createClickableText
+                ("[14D]", "/mp ipban " + ID + " 14d 使用第三方软件破坏游戏规则", "§e点击封禁14天");
+        BaseComponent[] BAN_30D = createClickableText
+                ("[30D]", "/mp ipban " + ID + " 30d 使用第三方软件破坏游戏规则", "§e点击封禁30天");
+        BaseComponent[] BAN_90D = createClickableText
+                ("[90D]", "/mp ipban " + ID + " 90d 使用第三方软件破坏游戏规则", "§e点击封禁90天");
+        BaseComponent[] BAN_360D = createClickableText
+                ("[360D]", "/mp ipban " + ID + " 360d 使用第三方软件破坏游戏规则", "§e点击封禁360天");
+        BaseComponent[] BAN_PERM = createClickableText
+                ("[永久]", "/mp ipban " + ID + " 使用第三方软件破坏游戏规则", "§e点击封禁永久");
         LineOne.addExtra(" ");
         LineOne.addExtra(BAN_7D[0]);
         LineOne.addExtra(" ");
@@ -75,31 +60,18 @@ public class PunishCMD extends Command implements TabExecutor {
         LineOne.addExtra(BAN_PERM[0]);
 
         BaseComponent LineTwo = new TextComponent("禁言类:");
-        BaseComponent[] MUTE_10MIN = new ComponentBuilder("[10MIN]")
-                .color(ChatColor.RED)
-                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/mp mute " + ID + " 10min 言论过激或违规, 请注意言行举止"))
-                .create();
-        BaseComponent[] MUTE_3H = new ComponentBuilder("[3H]")
-                .color(ChatColor.RED)
-                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/mp mute " + ID + " 3h 言论过激或违规, 请注意言行举止"))
-                .create();
-        BaseComponent[] MUTE_1D = new ComponentBuilder("[1D]")
-                .color(ChatColor.RED)
-                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/mp mute " + ID + " 1d 言论过激或违规, 请注意言行举止"))
-                .create();
-        BaseComponent[] MUTE_3D = new ComponentBuilder("[3D]")
-                .color(ChatColor.RED)
-                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/mp mute " + ID + " 3d 言论过激或违规, 请注意言行举止"))
-                .create();
-        BaseComponent[] MUTE_7D = new ComponentBuilder("[7D]")
-                .color(ChatColor.RED)
-                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/mp mute " + ID + " 7d 言论过激或违规, 请注意言行举止"))
-                .create();
-        BaseComponent[] MUTE_30D = new ComponentBuilder("[30D]")
-                .color(ChatColor.RED)
-                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/mp mute " + ID + " 30d 言论过激或违规, 请注意言行举止"))
-                .create();
-
+        BaseComponent[] MUTE_10MIN = createClickableText
+                ("[10MIN]", "/mp ipmute " + ID + " 10min 言辞过激或违规, 请注意言行举止", "§e点击禁言10分钟");
+        BaseComponent[] MUTE_3H = createClickableText
+                ("[3H]", "/mp ipmute " + ID + " 3h 言辞过激或违规, 请注意言行举止", "§e点击禁言3小时");
+        BaseComponent[] MUTE_1D = createClickableText
+                ("[1D]", "/mp ipmute " + ID + " 1d 言辞过激或违规, 请注意言行举止", "§e点击禁言1天");
+        BaseComponent[] MUTE_3D = createClickableText
+                ("[3D]", "/mp ipmute " + ID + " 3d 言辞过激或违规, 请注意言行举止", "§e点击禁言3天");
+        BaseComponent[] MUTE_7D = createClickableText
+                ("[7D]", "/mp ipmute " + ID + " 7d 言辞过激或违规, 请注意言行举止", "§e点击禁言7天");
+        BaseComponent[] MUTE_30D = createClickableText
+                ("[30D]", "/mp ipmute " + ID + " 30d 言辞过激或违规, 请注意言行举止", "§e点击禁言30天");
         LineTwo.addExtra(" ");
         LineTwo.addExtra(MUTE_10MIN[0]);
         LineTwo.addExtra(" ");
@@ -114,11 +86,19 @@ public class PunishCMD extends Command implements TabExecutor {
         LineTwo.addExtra(MUTE_30D[0]);
 
         sender.sendMessage(new TextComponent("§c---------------------------------"));
-        sender.sendMessage(new TextComponent("§e§l请选择你需要的处罚:"));
+        sender.sendMessage(new TextComponent("§e§l请选择你需要的处罚:" + " §b§l" + ID));
         sender.sendMessage(LineOne);
         sender.sendMessage(LineTwo);
         sender.sendMessage(new TextComponent("§c---------------------------------"));
 
+    }
+
+    private BaseComponent[] createClickableText(String text, String Command, String HoverText) {
+        return new ComponentBuilder(text)
+                .color(ChatColor.RED)
+                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, Command))
+                .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(HoverText).create()))
+                .create();
     }
 
     @Override
